4b4bc6e67d00071d33ca1fe16265f055e955f25d
Nicholas <ndrogaline@gmail.com>
2026-06-02 13:12:23 -0400

    BankAPI

---
+++ bankapp-api/src/main/java/com/example/demo/BankappApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankappApiApplication.class, args);
+	}
+
+}
